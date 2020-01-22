776d5fba027af258a94fd2feb61af2c5581f4b17
Ryan <ryan.fee625@gmail.com>
2020-01-22 08:27:28 -0500

    Update outputs.tf

--- outputs.tf
+++ outputs.tf
@@ -1,4 +1,24 @@
 output "s3_bucket_name" {
-  description = "S3_Bucket_Name"
+  description = "S3 Bucket Name"
   value       = aws_s3_bucket.scalr.bucket
+}
+
+output "s3_bucket_id" {
+  description = "S3 Bucket ID"
+  value       = aws_s3_bucket.scalr.id
+}
+
+output "s3_bucket_arn" {
+  description = "S3 Bucket ARN"
+  value       = aws_s3_bucket.scalr.arn
+}
+
+output "s3_bucket_domain_name" {
+  description = "S3 Bucket Domain Name"
+  value       = aws_s3_bucket.scalr.bucket_domain_name
+}
+
+output "s3_bucket_regional_domain_name" {
+  description = "S3 Bucket Regional Domain Name"
+  value       = aws_s3_bucket.scalr.bucket_regional_domain_name
 }
